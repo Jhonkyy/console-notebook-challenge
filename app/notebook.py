dca2f526b19833b60acec785941939231d81e734
Jhonkyyy <jhonfre0212@gmail.com>
2025-02-17 12:31:31 -0500

    class note created,and constants high, medium, low. And atributes code,title, text, importance, creation_date and tags

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,15 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+import datetime
+
+class Note:
+    HIGH: str = 'HIGH'
+    MEDIUM:str = 'MEDIUM'
+    LOW: str = 'LOW'
+    def __init__(self, code: str, title: str, text: str, importance: str):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.importance = importance
+
+        creation_date : datetime = datetime.now()
+        tags: list[str] = []
