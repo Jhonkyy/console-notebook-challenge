9af683c69865af71075e730597fbd755edb14b31
Jhonkyyy <jhonfre0212@gmail.com>
2025-02-17 13:03:02 -0500

    Notebook class added, and add note method

--- app/notebook.py
+++ app/notebook.py
@@ -1,15 +1,40 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
-import datetime
+from datetime import datetime
 
 class Note:
+
     HIGH: str = 'HIGH'
     MEDIUM:str = 'MEDIUM'
     LOW: str = 'LOW'
+
     def __init__(self, code: str, title: str, text: str, importance: str):
         self.code = code
         self.title = title
         self.text = text
         self.importance = importance
+        self.creation_date : datetime = datetime.now()
+        self.tags: list[str] = []
 
-        creation_date : datetime = datetime.now()
-        tags: list[str] = []
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self):
+        return f"Date: {self.creation_date}\n{self.title}: {self.text}"
+
+
+class Notebook:
+    def __init__(self):
+        self.notes: list[Note] = []
+
+    def add_note(self, title: str, text: str, importance: str) -> int:
+        new_code = len(self.notes) + 1
+        existing_codes = {note.code for note in self.notes}
+
+        while new_code in existing_codes:
+            new_code += 1
+
+        new_note = Note(new_code, title, text, importance)
+        self.notes.append(new_note)
+
+        return new_code
